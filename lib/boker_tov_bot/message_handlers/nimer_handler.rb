7015b2a1838b13da8f1545e066f6148506b08b52
Yoav <benjohusa@gmail.com>
2017-05-09 02:15:32 +0300

    tomash

---
+++ lib/boker_tov_bot/message_handlers/nimer_handler.rb
@@ -0,0 +1,23 @@
+require 'boker_tov_bot/message_handlers/base_handler'
+
+module BokerTovBot
+  module MessageHandlers
+    class NimrodHandler < BaseHandler
+      def initialize(options = {})
+        @regex = /.*(תום|שדמי).*/mi
+        @responses = ["אקסטרייייייייים", "תשמע הגזמנו אתמול", "וואה"]
+        super(options)
+      end
+
+      def match?(message)
+        @regex.match(message.downcase) ? true : false
+      end
+
+      def response(message)
+        reply_with_probability(0.3) do
+          [:text, @responses.sample]
+        end
+      end
+    end
+  end
+end
